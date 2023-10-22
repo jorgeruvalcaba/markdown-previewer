026f181e1a7dd16afa53fa09e1f00126472d9d60
Jorge Ruvalcaba <jorgeruvalcabav@gmail.com>
2023-10-21 23:21:41 -0600

    chore: retry update app

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -5,7 +5,7 @@ import Previewer from '../Previewer'
 
 import './styles.css'
 
-const App: React.FunctionComponent<{}> = () => {
+const App: React.FunctionComponent = () => {
   return (
     <div className="App">
       <h1>Markdown Previewer</h1>
